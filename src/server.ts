32010cda2a213ce133ff9886eda2c269767d377e
Elvis Farias <elvisrfarias1@gmail.com>
2025-06-21 15:40:45 -0300

    add docker

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,11 @@
-import { app } from "./app";
+import { app } from './app'
+import { env } from './env'
 
-
-app.listen({
-  host: '0.0.0.0',
-  port: 3335,
-}).then(() => {
-  console.log('HTTP server running!')
-}).catch((err) => {
-  console.error('Error starting server:', err);
-})
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('🚀 HTTP Server Running!')
+  })
